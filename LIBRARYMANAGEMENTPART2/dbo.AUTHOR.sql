54a877193e9c67535d4b4b8f8928261e38758981
JulyEd23 <33647199+JulyEd23@users.noreply.github.com>
2018-01-16 13:33:32 +0800

    Revert "jan. 11, 2018 input data commit"
    This reverts commit 62e6be7151b452602a894f9397836a9f21191dec.

--- LIBRARYMANAGEMENTPART2/dbo.AUTHOR.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[AUTHOR] (
-    [FirstName] VARCHAR (50) NOT NULL,
-    [ID]   INT          IDENTITY (1, 1) NOT NULL,
-    [LastName] VARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_AUTHOR] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
